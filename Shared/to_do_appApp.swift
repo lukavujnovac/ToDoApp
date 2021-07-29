bc86d97cf4451e2ba220efe65ab8764f7112edbb
Luka <vujnovacluka89@gmail.com>
2021-07-29 21:06:20 +0200

    Initial Commit

---
+++ Shared/to_do_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  to_do_appApp.swift
+//  Shared
+//
+//  Created by Luka Vujnovac on 29.07.2021..
+//
+
+import SwiftUI
+
+@main
+struct to_do_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
